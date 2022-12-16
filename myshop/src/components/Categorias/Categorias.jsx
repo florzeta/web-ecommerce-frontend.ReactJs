97bbdaafb23d24f1c6a38b36d6b546e91213dec0
florzeta <99660646+florzeta@users.noreply.github.com>
2022-12-15 22:12:27 -0300

    Navbar

---
+++ myshop/src/components/Categorias/Categorias.jsx
@@ -0,0 +1,21 @@
+
+const Categorias = () => {
+    return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+                <button className='btn btn-secundary'><i className="fas fa-home fa-lg"></i></button>
+            </li>
+            <li className="nav-item">
+                <button className='btn btn-secundary' >Lentes</button>
+            </li>
+            <li className="nav-item">
+                <button className='btn btn-secundary' >Bolsos</button>
+            </li>
+            <li className="nav-item">
+                <button className='btn btn-secundary' >Accesorios</button>
+            </li>
+      </ul>
+    );
+}
+
+export default Categorias;
